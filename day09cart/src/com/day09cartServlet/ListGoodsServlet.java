36dae83b8f57ce523e1efdc22c3d3c3ae1ff02eb
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-28 20:15:47 +0800

    Update ListGoodsServlet.java

--- day09cart/src/com/day09cartServlet/ListGoodsServlet.java
+++ day09cart/src/com/day09cartServlet/ListGoodsServlet.java
@@ -19,7 +19,7 @@ import com.day09cartBase.Goods;
 /**
  * Servlet implementation class ListGoodsServlet
  */
-@WebServlet("/goods/list")
+@WebServlet("/goods/list") 
 public class ListGoodsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
