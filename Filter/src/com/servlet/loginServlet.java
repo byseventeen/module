4fe5796307f41c862a40df833726ad3205beee68
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-29 22:48:44 +0800

    Update loginServlet.java

--- Filter/src/com/servlet/loginServlet.java
+++ Filter/src/com/servlet/loginServlet.java
@@ -20,7 +20,9 @@ public class loginServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// 获取参数
+		String userName=request.getParameter("userName");
+		String userPass=request.getParameter("userPass");
 	}
 
 }
