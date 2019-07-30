fdacee8bfd64af3556f0ca9e1c7ccffb5bc03b60
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-30 20:36:32 +0800

    Update MyServletContextListener.java

--- CreateTable/src/com/createTable/listener/MyServletContextListener.java
+++ CreateTable/src/com/createTable/listener/MyServletContextListener.java
@@ -3,18 +3,24 @@ package com.createTable.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.createTable.entity.Employee;
+
 public class MyServletContextListener implements ServletContextListener {
 
-	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// 监听器加载的时候执行的方法
+		
+		//获取Employee类的Class对象
+		  Class employee = Employee.class;
+		  employee.getDeclaredFields();
+
+	}
+	
 	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
